85f6c0ff244e8fcbb9667aecfaf86a7d1a3474c1
gracebir <grace.devolop@gmail.com>
2022-11-07 13:43:03 +0200

    adding te config env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/Auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, PrismaModule, BookmarkModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    AuthModule, 
+    PrismaModule, 
+    BookmarkModule
+  ],
 })
 export class AppModule {}
